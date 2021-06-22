cb8a7e8cb020f2b6bfb0c99f8d33a7e61650a953
chavda-bhavik <bhavik.chvda@gmail.com>
2021-06-22 20:22:00 +0530

    grouped by journals to show journals and journal-title dynamically

--- src/pages/home/Journals/index.tsx
+++ src/pages/home/Journals/index.tsx
@@ -1,29 +1,52 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useLocation } from 'wouter';
 import { Journal } from '../Journal';
 import { Title } from '../Title';
+import dayjs from 'dayjs';
+import { useState } from 'react';
 
 interface JournalsProps {
     journals: FormattedJournalType[];
 }
+type GroupedJournalsType = Record<string, FormattedJournalType[]>;
 
 export const Journals: React.FC<JournalsProps> = ({ journals }) => {
     const [, setLocation] = useLocation();
+    const [groupedJournals, setGroupedJournals] = useState<GroupedJournalsType>({});
+
+    useEffect(() => {
+        let dateStr = '';
+        let groupedJournalsGroupByDates = journals.reduce((obj, journal) => {
+            dateStr = dayjs(Number(journal.date)).format('MMMM YYYY');
+            obj.set(dateStr, [...(obj.get(dateStr) || []), journal]);
+            return obj;
+        }, new Map());
+        let grpdJouranals = Object.fromEntries(groupedJournalsGroupByDates);
+        setGroupedJournals(grpdJouranals);
+    }, [journals]);
+
     const onJournalClick = (journal: FormattedJournalType) => {
         setLocation(`/view/${journal.id}`);
     };
+
     return (
         <div className="px-3">
-            <Title title="May 2021" />
+            {Object.keys(groupedJournals).reduce((journalData: any[], journalDateTitle) => {
+                journalData.push(<Title title={journalDateTitle} key={journalDateTitle} />);
 
-            {journals.map((journal, i) => (
-                <Journal
-                    journal={journal}
-                    hasConnector={i !== journals.length - 1}
-                    key={journal.id}
-                    onClick={onJournalClick}
-                />
-            ))}
+                groupedJournals[journalDateTitle].forEach((journal, i) => {
+                    journalData.push(
+                        <Journal
+                            journal={journal}
+                            hasConnector={i !== groupedJournals[journalDateTitle].length - 1}
+                            key={journal.id}
+                            onClick={onJournalClick}
+                        />,
+                    );
+                });
+                return journalData;
+            }, [])}
         </div>
     );
 };
