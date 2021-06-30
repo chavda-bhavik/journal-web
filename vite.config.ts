2f027444e91f5100ddfd9f069ea6abb9384057cc
chavda-bhavik <bhavik.chvda@gmail.com>
2021-06-30 16:55:50 +0530

    added PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,39 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
         brotliSize: false,
     },
-    plugins: [tsconfigPaths(), reactRefresh()],
+    plugins: [
+        tsconfigPaths(),
+        reactRefresh(),
+        VitePWA({
+            workbox: {
+                additionalManifestEntries: [
+                    // eslint-disable-next-line unicorn/no-null
+                    { url: 'https://rsms.me/inter/inter.css', revision: null },
+                ],
+                cleanupOutdatedCaches: true,
+                clientsClaim: true,
+                skipWaiting: true,
+                navigateFallback: undefined,
+            },
+            manifest: {
+                name: 'five minute journal lite',
+                short_name: 'Lite',
+                theme_color: '#EDEDE5',
+                icons: [
+                    {
+                        src: '/icon_512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
+        }),
+    ],
 });
