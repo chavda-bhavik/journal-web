8facd96014f83e3ec4542f82e953a9c84c48fe6d
chavda-bhavik <bhavik.chvda@gmail.com>
2021-07-03 09:30:54 +0530

    added redux-persist to store journals state

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,37 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { useDispatch, TypedUseSelectorHook, useSelector } from 'react-redux';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+    PersistConfig,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 // reducers
 import journalReducer from './journal';
 
-const store = configureStore({
-    reducer: {
-        journal: journalReducer,
-    },
+const persistConfig = { key: 'root', version: 1, storage, whitelist: ['journal'] };
+
+const rootReducer = combineReducers({
+    journal: journalReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware({
+        serializableCheck: { ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER] },
+    }),
+});
+
+export const persistor = persistStore(store);
 
 export default store;
 
